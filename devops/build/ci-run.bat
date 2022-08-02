ded9b999b2a82184aa645575be791fe06546613c
Frank Quinn <fquinn@cascadium.io>
2022-08-02 08:25:38 +0100

    Fixed zip archiving for windows

--- devops/build/ci-run.bat
+++ devops/build/ci-run.bat
@@ -11,7 +11,7 @@ cd %GITHUB_WORKSPACE%\build || goto error
 cmake -G "%GENERATOR%" %EXTRA_ARGS% -DCMAKE_TOOLCHAIN_FILE=c:/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=%OPENMAMA_INSTALL_DIR% -DGTEST_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DMAMA_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ .. || goto error
 cmake --build . --config RelWithDebInfo --target install || goto error
 ctest . -C RelWithDebInfo  -E MsgFieldVectorBoolTests.GetVectorBoolNullField --timeout 120 --output-on-failure || goto error
-
+cd %GITHUB_WORKSPACE% || goto error
 7z a openmama-omnm.%VCVER%.%PLATFORM%.zip "%OPENMAMA_INSTALL_DIR%" || goto error
 
 goto end
